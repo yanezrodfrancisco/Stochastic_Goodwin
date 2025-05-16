cfaaa98a8393b09c7f3dda672bca158faf44fbd7
Francisco Yáñez Rodriguez <francisco.yanez@rai.usc.es>
2025-05-16 10:11:37 +0200

    Update noise_library.py

--- noise_library.py
+++ noise_library.py
@@ -54,7 +54,7 @@ def HeunPred(f, g, x, sg, noise, dt, params):
      return f(x, params)+(g(x, params)*sg*noise)*dt**-0.5
 
 @numba.njit()
-def Heun_solution(T, dt, var, params, sg1, pasos, mid_step:int=100):
+def Heun_solution(T, dt, var, params, sg1, noise_frec, mid_step:int=100):
 
      rho, gamma, kappa, lamda, alpha, sigma, delta, mu, bu, bv= params
      NoiseP=np.random.normal(0, 1, T)
@@ -70,15 +70,15 @@ def Heun_solution(T, dt, var, params, sg1, pasos, mid_step:int=100):
 
      for i in range(int(mid_step*(T-1))):
          x_0=np.array([U[i//mid_step], V[i//mid_step], P[i//mid_step], mu])
-         predictor=np.array([HeunPred(fu, gu,   x_0, sg1, NoiseP[i//pasos], dt, params),
-                             HeunPred(fv, gv,   x_0, sg1, NoiseP[i//pasos], dt, params),
-                             HeunPred(fp, one,  x_0, sg1, NoiseP[i//pasos], dt, params),
-                             HeunPred(fn, zero, x_0, sg1, NoiseP[i//pasos], dt, params)])
+         predictor=np.array([HeunPred(fu, gu,   x_0, sg1, NoiseP[i//noise_frec], dt, params),
+                             HeunPred(fv, gv,   x_0, sg1, NoiseP[i//noise_frec], dt, params),
+                             HeunPred(fp, one,  x_0, sg1, NoiseP[i//noise_frec], dt, params),
+                             HeunPred(fn, zero, x_0, sg1, NoiseP[i//noise_frec], dt, params)])
          
-         U[(i+1)//mid_step]=x_0[0]+0.5*dt*(predictor[0]+HeunPred(fu, gu,  x_0+dt*predictor, sg1, NoiseP[i//pasos], dt, params))
-         V[(i+1)//mid_step]=x_0[1]+0.5*dt*(predictor[1]+HeunPred(fv, gv,  x_0+dt*predictor, sg1, NoiseP[i//pasos], dt, params))
-         P[(i+1)//mid_step]=x_0[2]+0.5*dt*(predictor[2]+HeunPred(fp, one, x_0+dt*predictor, sg1, NoiseP[i//pasos], dt, params))
-         Noise[(i+1)//mid_step]=NoiseP[i//pasos]*sg1 + delta
+         U[(i+1)//mid_step]=x_0[0]+0.5*dt*(predictor[0]+HeunPred(fu, gu,  x_0+dt*predictor, sg1, NoiseP[i//noise_frec], dt, params))
+         V[(i+1)//mid_step]=x_0[1]+0.5*dt*(predictor[1]+HeunPred(fv, gv,  x_0+dt*predictor, sg1, NoiseP[i//noise_frec], dt, params))
+         P[(i+1)//mid_step]=x_0[2]+0.5*dt*(predictor[2]+HeunPred(fp, one, x_0+dt*predictor, sg1, NoiseP[i//noise_frec], dt, params))
+         Noise[(i+1)//mid_step]=NoiseP[i//noise_frec]*sg1 + delta
 
 
      return U, V, P, Noise
